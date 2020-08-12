00917a66b1debd3a0493b8cc5350b32b7a3e2810
dodisutarya <9284479+dodisutarya@users.noreply.github.com>
2020-08-12 10:15:03 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+ReactDOM.render(<h1>hello</h1>, document.getElementById("root"));
